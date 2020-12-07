3f42b38d8758c6c25e70b238c6f96ff4fd4cf2cd
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-07 14:19:11 +0500

    Main dart file has been enabled

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,27 @@ void main() => runApp(
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return Ball();
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blue,
       appBar: AppBar(
         backgroundColor: Colors.indigo,
         title: Text('Ask Me Anything'),
       ),
-      body: Container(),
+      body: Container(
+        child: Image.asset('images/ball1.png'),
+      ),
     );
   }
 }
