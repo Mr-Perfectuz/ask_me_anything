6bc2594c2311ff1fcb7600a6135985cba13f81ef
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2020-12-07 13:27:34 +0500

    Main dart file has been enabled

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Container(),
+    );
+  }
+}
